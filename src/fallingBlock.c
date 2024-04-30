957455a9d1eb1096c3215e727c9b5c5b6e376211
Julia Kardasz <juliakardasz.jk@gmail.com>
2024-04-30 14:25:39 +0200

    c to gowno

--- src/fallingBlock.c
+++ src/fallingBlock.c
@@ -3,28 +3,34 @@
 #include <time.h>
 #include <string.h>
 #include <signal.h>
-#include<stdlib.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include<ctype.h>
 
+ extern int action_code_for_falling_block;
 
 void fallingBlock(){
     printf("weszlo\n");
     clock_t start=clock();
-    char choice = 'K';
-    // while(choice=getch() && clock()-start<10000){    
-    switch(getchar()){
+    char choice = 'K';  
+    
+    switch(tolower(getchar())){
         case 'a':
-            printf("przesuniety");
+        //left
+            action_code_for_falling_block=1;
             break;
         case 'd':
             //right
+            action_code_for_falling_block=2;
             break;
         case 'w':
+            action_code_for_falling_block=3;
             break;
             //rotation
     }
     printf("wyszlo\n");
-    alarm(10);    
-    signal(SIGALRM, fallingBlock);
+    return;
+    // alarm(10);    
+    // signal(SIGALRM, fallingBlock);
 
 }
--- src/main.c
+++ src/main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
+#include <ncurses.h> 
+#include<stdlib.h>
+#include<ctype.h>
+
 #include "../include/fallingBlock.h"
+
+int action_code_for_falling_block=0;
 
 void update(int plansza[8][16]){
 	sleep(100);
 	printf("k\n");
 	return;
 }
-
 
 
 void display_message(int s)
@@ -26,10 +31,23 @@ int main(){
 		for(j = 0; j < 16; j++) plansza[i][j] = 0; //zerowanie na poczatku
 	}
 	// while (1) update(plansza);
-	//fallingBlock();
+	while(1){
+
+	
+	initscr();
+	refresh();
+	action_code_for_falling_block=0;
+	fallingBlock();
 	signal(SIGALRM, fallingBlock);
     alarm(10);     // Initial timeout setting
-
-       pause();
+	endwin();
+	if(action_code_for_falling_block==1){ // przesuniecie w lewo
+		printf("lewo");
+	}else if(action_code_for_falling_block==2){
+		printf("prawo");
+	}
+	}
+	//printf("c to gowno");
+    //    pause();
     return 0;
 }
