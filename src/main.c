a0ed950bad1126c87f181a2d6f83d870ee38d988
szynek <wojtow.szym@gmail.com>
2024-04-30 12:09:38 +0200

    ok3

--- src/main.c
+++ src/main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-
+#include<system.h>
 #include "../include/fallingBlock.h"
 
-void update(){
-
+void update(int plansza[8][16]){
+	sleep(100);
+	printf("k\n");
+	return;
 }
 
 int main(){
     printf("%s", "ok");
-    update();
+	int plansza[8][16];
+	while (1) update(plansza);
     return 0;
 }
